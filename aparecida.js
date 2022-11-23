53bb8ac3cc696207c6719dfd268ee79ba5d1da14
l30n44rd0std <116068969+apa2467@users.noreply.github.com>
2022-11-23 14:45:41 -0300

    modificações
    add código de salário

---
+++ aparecida.js
@@ -0,0 +1,23 @@
+
+/* exemplo 1 */
+let minhaIdade = 25;
+
+let idadeLimite = 100;
+
+if (minhaIdade > idadeLimite) {
+    console.log("Acima da idade limite");
+} else {
+    console.log("Abaixo da idade limite");
+}
+
+/*exemplo 2*/
+
+let meuSalario = 500;
+
+
+if (meuSalario < 1500) {
+    console.log("Você ")
+
+}
+
+
