570cbcc1349303221748bc6d2c3786c472086a01
Iderlan <118554164+IderlanOliveira@users.noreply.github.com>
2022-11-23 15:05:26 -0300

    add
    1234

--- iderlan.js
+++ iderlan.js
@@ -7,3 +7,5 @@ document.write("David Professor TOPADO")
 alert("Olá mundo!")
 
 document.write("Se a vida for, ela realmente ira!")
+
+document.write("Dilma: Quem ganhar não vai perder, quem perder não vai perder, no final vamos todos perder!")
